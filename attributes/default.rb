bd9e41b7924f3d506abed9fe4ebc74e2f402cbf8
galstrom21 <jshepher@rackspace.com>
2012-08-18 13:53:00 -0500

    Adding CentOS support
    fixes #4

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ case node["platform"]
       "sosreport_packages" => [ "sos" ],
       "package_overrides" => "-o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-confdef'"
     }
-  when "fedora", "redhat"
+  when "fedora", "redhat", "centos"
     default["sosreport"]["platform"] = {
       "sosreport_packages" => [ "sos" ],
       "package_overrides" => ""
