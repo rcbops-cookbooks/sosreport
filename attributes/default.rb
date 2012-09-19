31c24a313276ac794c67ddfa092abe5d3931c252
Darren Birkett <darren.birkett@gmail.com>
2012-09-19 11:43:05 +0100

    classifying attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,12 @@
 
 case node["platform"]
   when "ubuntu"
-    default["sosreport"]["platform"] = {
+    default["sosreport"]["platform"] = {        # node_attribute
       "sosreport_packages" => [ "sos" ],
       "package_overrides" => "-o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-confdef'"
     }
   when "fedora", "redhat", "centos"
-    default["sosreport"]["platform"] = {
+    default["sosreport"]["platform"] = {        # node_attribute
       "sosreport_packages" => [ "sos" ],
       "package_overrides" => ""
     }
